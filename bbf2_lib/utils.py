67a9c3321744e875b5caf5d744bcbda689cd3e53
tommyippoz <tommaso.zoppi@unifi.it>
2026-05-22 16:18:55 +0200

    first release

--- bbf2_lib/utils.py
+++ bbf2_lib/utils.py
@@ -4,83 +4,18 @@ import os
 import time
 
 import numpy
-import pandas
 import sklearn.metrics
+from sklearn.metrics import confusion_matrix
+
+from bbf2_lib.AnomalyPredictor import AnomalyPredictor, TimeSeriesAnomalyPredictor
 
 
-def get_classifier_name(clf_obj) -> str:
-    """
-    Gets the name of a classifier object
-    :param clf_obj:
-    :return:
-    """
-    invert_op = getattr(clf_obj, "classifier_name", None)
-    if callable(invert_op):
-        return clf_obj.classifier_name()
-    else:
-        return clf_obj.__class__.__name__
+# ----------------------------------------------------
+#          FILE CONTAINING UTILITY FUNCTIONS
+# ----------------------------------------------------
 
 
-def load_dataset_config(cfg_file):
-    """
-    Loads parameters for execution from a configuration file. Specific for dataset loaders
-    :param cfg_file: the path to the configuration file
-    :return: a dictionary
-    """
-    # Loading Conf file
-    if not os.path.exists(cfg_file):
-        return None
-    config = configparser.ConfigParser()
-    config.read(cfg_file)
-
-    # Setting up variables
-    cfg_info = {}
-    if config is not None:
-        # Reading CSV info
-        if ('csv_path' in config['base']) and len(config['base']['csv_path'].strip()) > 0 \
-                and os.path.exists(config['base']['csv_path'].strip()):
-            cfg_info['csv_path'] = config['base']['csv_path'].strip()
-        else:
-            cfg_info['csv_path'] = None
-        if ('kwh_col' in config['base']) and len(config['base']['kwh_col'].strip()) > 0:
-            cfg_info['kwh_col'] = config['base']['kwh_col'].strip()
-        else:
-            cfg_info['kwh_col'] = None
-        if ('time_col' in config['base']) and len(config['base']['time_col'].strip()) > 0:
-            cfg_info['time_col'] = config['base']['time_col'].strip()
-        else:
-            cfg_info['time_col'] = None
-        if ('seq_col' in config['base']) and len(config['base']['seq_col'].strip()) > 0:
-            cfg_info['seq_col'] = config['base']['seq_col'].strip()
-        else:
-            cfg_info['seq_col'] = None
-        if ('batch_size' in config['base']) and len(config['base']['batch_size'].strip()) > 0:
-            cfg_info['batch_size'] = int(config['base']['batch_size'].strip())
-        else:
-            cfg_info['batch_size'] = -1
-        if ('limit_rows' in config['base']) and len(config['base']['limit_rows'].strip()) > 0:
-            cfg_info['limit_rows'] = int(config['base']['limit_rows'].strip())
-        else:
-            cfg_info['limit_rows'] = -1
-
-            # Threats
-        cfg_info['threats'] = []
-        for thr, flag in config.items("threats"):
-            if flag:
-                cfg_info['threats'].append(thr)
-
-        # Injection
-        if ('duration' in config['injection']) and len(config['injection']['duration'].strip()) > 0:
-            cfg_info['duration'] = int(config['injection']['duration'].strip())
-        if ('min_normal' in config['injection']) and len(config['injection']['min_normal'].strip()) > 0:
-            cfg_info['min_normal'] = int(config['injection']['min_normal'].strip())
-        if ('perc_inj' in config['injection']) and len(config['injection']['perc_inj'].strip()) > 0:
-            cfg_info['perc_inj'] = float(config['injection']['perc_inj'].strip())
-
-    return cfg_info
-
-
-def read_general_conf(cfg_file):
+def read_config(cfg_file):
     """
     Loads parameters for execution from a configuration file.
     :param cfg_file: the path to the configuration file
@@ -94,79 +29,16 @@ def read_general_conf(cfg_file):
 
     # Setting up variables
     cfg_info = {}
-    if config is not None:
-        # Reading folders info
-        if ('cfg_files_folder' in config['folders']) and len(config['folders']['cfg_files_folder'].strip()) > 0:
-            cfg_info['input_folder'] = config['folders']['cfg_files_folder'].strip()
-            if not os.path.exists(cfg_info['input_folder']):
-                os.mkdir(cfg_info['input_folder'])
-        else:
-            cfg_info['input_folder'] = None
-        if ('models_folder' in config['folders']) and len(config['folders']['models_folder'].strip()) > 0:
-            cfg_info['models_folder'] = config['folders']['models_folder'].strip()
-            if not os.path.exists(cfg_info['models_folder']):
-                os.mkdir(cfg_info['models_folder'])
-        else:
-            cfg_info['models_folder'] = None
-        if ('out_folder' in config['folders']) and len(config['folders']['out_folder'].strip()) > 0:
-            cfg_info['output_folder'] = config['folders']['out_folder'].strip()
-            if not os.path.exists(cfg_info['output_folder']):
-                os.mkdir(cfg_info['output_folder'])
-        else:
-            cfg_info['output_folder'] = None
-        if ('gen_data_tag' in config['folders']) and len(config['folders']['gen_data_tag'].strip()) > 0:
-            cfg_info['data_tag'] = config['folders']['gen_data_tag'].strip()
-        else:
-            cfg_info['data_tag'] = 'generated'
-
-        # Reading trainer info
-        if ('tt_split' in config['trainer']) and len(config['trainer']['tt_split'].strip()) > 0:
-            cfg_info['tt_split'] = float(config['trainer']['tt_split'].strip())
-        else:
-            cfg_info['tt_split'] = 0.5
-        if ('force_binary' in config['trainer']) and len(config['trainer']['force_binary'].strip()) > 0:
-            fb_text = config['trainer']['force_binary'].strip()
-            cfg_info['force_binary'] = True if fb_text in ['1', 'True', 'true', 'yes', 'Y', 'T'] else False
-        else:
-            cfg_info['force_binary'] = False
+    for section in config.sections():
+        cfg_info[section] = {}
+        for option in config.options(section):
+            cfg_info[option] = config.get(section, option)
+            if "," in cfg_info[option]:
+                cfg_info[option] = [x.strip() for x in cfg_info[option].split(',')]
+            elif cfg_info[option] in ["True", "False", "true", "false", "t", "f", "T", "F"]:
+                cfg_info[option] = True if cfg_info[option].upper().startswith("T") else False
 
     return cfg_info
-
-
-def print_sequences(filename: str, seq_list, create_new=True):
-    """
-    Prints a list of sequences to a file
-    :param filename: name of the file
-    :param seq_list: list of sequences to print
-    :param create_new: True if the files needs to be created, otherwise it appends
-    """
-
-    open_flag = 'w' if create_new else 'a'
-    with open(filename, open_flag) as f:
-        if open_flag == 'w':
-            f.write('seq_name,timestamp,value,label\n')
-        for seq in seq_list:
-            if seq is not None:
-                for i in range(0, seq.length()):
-                    element = seq.get_i(i)
-                    f.write("%s,%s,%f,%s\n" % (seq.get_tag(), element[0], element[1], element[2]))
-
-
-def print_full_sequences(filename: str, seq_list, create_new=True):
-    """
-    Prints a list of sequences to a file
-    :param filename: name of the file
-    :param seq_list: list of sequences to print
-    :param create_new: True if the files needs to be created, otherwise it appends
-    """
-
-    open_flag = 'w' if create_new else 'a'
-    for seq in seq_list:
-        if seq is not None:
-            to_write = seq.get_all_data()
-            to_write['seq_name'] = [seq.tag for _ in to_write.index]
-            to_write.to_csv(filename, mode=open_flag, index=False, header=(False if open_flag == 'a' else True))
-            open_flag = 'a'
 
 
 def current_ms():
@@ -177,24 +49,8 @@ def current_ms():
     return round(time.time() * 1000)
 
 
-def compute_stats(clf_y, ad_y, test_y, test_seq: list = None, diagnosis_time: int = 1):
-    """
-    Computes stats for both the classifier and the anomaly detector
-    :param clf_y: scores of the classifier
-    :param ad_y: scores of the detector (including policy)
-    :param test_y: ground truth
-    :return: a dictionary
-    """
-    stats = {}
-    classes = numpy.unique(test_y)
-    stats['clf'] = compute_single_stats(clf_y, test_y, classes)
-    stats['ad'] = compute_single_stats(ad_y, test_y, classes, test_seq, diagnosis_time)
-    stats['classes'] = "-".join([x for x in classes])
-    return stats
-
-
-def compute_single_stats(pred_y, test_y, classes, test_sequences=None,
-                         diagnosis_time: int = 1, max_delay: int = 50):
+def score_metrics(pred_y, test_y, test_sequences=None,
+                         diagnosis_time: int = 1, max_delay: int = 50) -> dict:
     """
     Computes stats for predicted labels
     :param pred_y: scores of the classifier
@@ -202,30 +58,29 @@ def compute_single_stats(pred_y, test_y, classes, test_sequences=None,
     :param classes: problem's classes
     :return: a dictionary
     """
-    stats = {}
-    stats['accuracy'] = sklearn.metrics.balanced_accuracy_score(test_y, pred_y)
-    stats['mcc'] = sklearn.metrics.matthews_corrcoef(test_y, pred_y)
-    stats['recall_class'] = dict(zip(classes,
-                                     sklearn.metrics.recall_score(test_y, pred_y,
-                                                                  labels=classes, average=None)))
+    stats = {"point":{}, "timeseries":{}}
+    stats['point']['tn'], stats['point']['fp'], stats['point']['fn'], stats['point']['tp'] = (
+        confusion_matrix(test_y, pred_y).ravel())
+    stats['point']['accuracy'] = sklearn.metrics.balanced_accuracy_score(test_y, pred_y)
+    stats['point']['mcc'] = sklearn.metrics.matthews_corrcoef(test_y, pred_y)
+    stats['point']['recall'] = sklearn.metrics.recall_score(test_y, pred_y)
     if test_sequences is not None:
-        normal_class = 'normal'
+        normal_class = 0
         fprs = []
         dds = []
-        dds_per_class = {x: [] for x in classes}
         tprs = []
-        tprs_per_class = {x: [] for x in classes}
         index = 0
         for seq in test_sequences:
 
+            seq_data = seq["X"]
+            seq_labels = seq["Y"]
             # Deriving sequence-related scores
-            seq_pred = pred_y[index:index + seq.length()]
-            seq_label = test_y[index:index + seq.length()]
-            index += seq.length()
+            seq_pred = pred_y[index:index + len(seq_labels)]
+            seq_label = test_y[index:index + len(seq_labels)]
+            index += len(seq_labels)
 
             # Iterating over sequence data
             cooldown = 0
-            det_label = ''
             an_time = -1
             det_time = -1
             false_alerts = []
@@ -238,7 +93,6 @@ def compute_single_stats(pred_y, test_y, classes, test_sequences=None,
                         false_alerts.append(i)
                 if an_time < 0 and seq_label[i] != normal_class:
                     an_time = i
-                    det_label = seq_label[i]
                 if cooldown > 0:
                     cooldown -= 1
 
@@ -246,35 +100,79 @@ def compute_single_stats(pred_y, test_y, classes, test_sequences=None,
             fprs.append(len(false_alerts) / (seq_label == normal_class).sum())
             detected = 1 if det_time > 0 and not math.isnan(det_time) else 0
             tprs.append(detected)
-            tprs_per_class[det_label].append(detected)
             if detected > 0:
                 dds.append((det_time - an_time) if an_time <= det_time and not math.isnan(det_time) else max_delay)
-                dds_per_class[det_label].append(
-                    (det_time - an_time) if an_time <= det_time and not math.isnan(det_time) else max_delay)
 
-        stats['overall_fpr'] = {'avg': float(numpy.average(fprs)), 'min': float(numpy.min(fprs)),
+        stats['timeseries']['fpr'] = {'avg': float(numpy.average(fprs)), 'min': float(numpy.min(fprs)),
                                 'max': float(numpy.max(fprs)), 'median': float(numpy.median(fprs)),  # 'all': fprs
                                 }
-        stats['overall_tpr'] = {'avg': float(numpy.average(tprs)), 'min': int(numpy.min(tprs)),
+        stats['timeseries']['tpr'] = {'avg': float(numpy.average(tprs)), 'min': int(numpy.min(tprs)),
                                 'max': int(numpy.max(tprs)), 'median': int(numpy.median(tprs)),  # 'all': tprs
                                 }
-        stats['overall_dd'] = {'avg': float(numpy.nanmean(dds)), 'min': int(numpy.nanmin(dds)),
+        stats['timeseries']['dd'] = {'avg': float(numpy.nanmean(dds)), 'min': int(numpy.nanmin(dds)),
                                'max': int(numpy.nanmax(dds)), 'median': int(numpy.nanmedian(dds)),  # 'all': dds
                                } if len(dds) > 0 else {'avg': -1, 'min': -1, 'max': -1, 'median': -1}
-        stats['per_class_tpr'] = {}
-        stats['per_class_dd'] = {}
-        for an_class in tprs_per_class.keys():
-            class_tpr = tprs_per_class[an_class]
-            stats['per_class_tpr'][an_class] = {'avg': float(numpy.average(class_tpr)) if len(class_tpr) > 0 else -1,
-                                                'min': int(numpy.min(class_tpr)) if len(class_tpr) > 0 else -1,
-                                                'max': int(numpy.max(class_tpr)) if len(class_tpr) > 0 else -1,
-                                                'median': int(numpy.median(class_tpr)) if len(class_tpr) > 0 else -1,  # 'all': tprs
-                                                }
-            class_dd = dds_per_class[an_class]
-            stats['per_class_dd'][an_class] = {'avg': float(numpy.nanmean(class_dd)) if len(class_dd) > 0 else -1,
-                                               'min': int(numpy.nanmin(class_dd)) if len(class_dd) > 0 else -1,
-                                               'max': int(numpy.nanmax(class_dd)) if len(class_dd) > 0 else -1,
-                                               'median': int(numpy.nanmedian(class_dd)) if len(class_dd) > 0 else -1,  # 'all': dds
-                                               } if len(class_dd) > 0 else {'avg': -1, 'min': -1, 'max': -1, 'median': -1}
 
     return stats
+
+def test_models(predictor: AnomalyPredictor, test_sequences:list, verbose:bool = True) -> list:
+    """
+    Tests a group of models trained with an AnomalyPredictor
+    :param predictor: the anomalypredictor of choice (has to be fitted already)
+    :param test_sequences: test sequences
+    :param verbose: True if debug information has to be shown
+    :return: a list of dict containing metrics
+    """
+    y_test = numpy.concatenate([numpy.asarray(item["Y"], dtype=int) for item in test_sequences], axis=0)
+    test_results = predictor.predict(sequences=test_sequences)
+    if verbose:
+        print("----- PREDICTION RESULTS -----")
+    metrics_list = []
+    for result in test_results:
+        metrics = score_metrics(result["predictions"], y_test, test_sequences)
+        metrics["general"] = {"clf": result["clf"], "predict_time": result["predict_time"],
+                              "predict_time_per_item": result["predict_time_per_item"],
+                              "use_timeseries":isinstance(predictor, TimeSeriesAnomalyPredictor),
+                              "supervised": predictor.supervised}
+        metrics_list.append(metrics)
+        if verbose:
+            print("Classifier %s predicted with accuracy %.4f" % (result["clf"], metrics["point"]["accuracy"]))
+
+    return metrics_list
+
+
+def print_scores(to_print, analysis_tag:str, output_folder, filename):
+    """
+    Prints scores of test runs to file specified in the CFG
+    :param to_print: data to print
+    :param output_folder: folder in which outputs are placed
+    :param filename: name of the output file
+    :return:
+    """
+    if not os.path.exists(output_folder):
+        os.mkdir(output_folder)
+    csv_file = os.path.join(output_folder, filename).replace('"', '')
+    if not os.path.exists(csv_file):
+        with open(csv_file, 'w') as f:
+            f.write("timeseries,supervised,analysis_tag,clf_name,tp,fp,fn,tn,mcc,acc,recall,test_time,test_time_per_item,dd_min,dd_max,dd_avg,fpr_min,fpr_max,fpr_avg,tpr_min,tpr_max,tpr_avg\n")
+    for result in to_print:
+        with open(csv_file, "a") as myfile:
+            myfile.write(str(result['general']['use_timeseries']) + "," +
+                         str(result['general']['supervised']) + "," +
+                         analysis_tag + "," + result['general']['clf'] + "," +
+                         str(result['point']['tp']) + "," + str(result['point']['fp']) + "," +
+                         str(result['point']['fn']) + "," + str(result['point']['tn']) + "," +
+                         str(result['point']['mcc']) + "," + str(result['point']['accuracy']) + "," +
+                         str(result['point']['recall']) + ',' + str(result['general']['predict_time']) + ',' +
+                         str(result['general']['predict_time_per_item']) + "," +
+                         str(result['timeseries']['dd']['min']) + "," +
+                         str(result['timeseries']['dd']['max']) + "," +
+                         str(result['timeseries']['dd']['avg']) + "," +
+                         str(result['timeseries']['fpr']['min']) + "," +
+                         str(result['timeseries']['fpr']['max']) + "," +
+                         str(result['timeseries']['fpr']['avg']) + "," +
+                         str(result['timeseries']['tpr']['min']) + "," +
+                         str(result['timeseries']['tpr']['max']) + "," +
+                         str(result['timeseries']['tpr']['avg']) + "\n")
+
+    pass
---
+++ debug/learn_anomaly_detectors.py
@@ -0,0 +1,90 @@
+import random
+from pathlib import Path
+
+import numpy
+import pandas
+
+from bbf2_lib.AnomalyPredictor import PointWiseAnomalyPredictor, TimeSeriesAnomalyPredictor
+from bbf2_lib.Classifier import choose_classifiers
+from bbf2_lib.utils import read_config, test_models, print_scores
+
+if __name__ == "__main__":
+    """
+    This main performs an analysis of CSV files extracted from commaai dataset
+    Parameter configuration via "debug_cfg.cfg" file
+    """
+
+    # Reads params file
+    params = read_config("debug_cfg.cfg")
+    if params is None:
+        print("Cannot read CFG file. Please provide a debug_cfg.cfg file in the same folder of this script.")
+        exit(1)
+
+    # Sets random seed to increase repeatability
+    random.seed(int(params["rng_seed"]))
+    numpy.random.seed(int(params["rng_seed"]))
+    random_state = int(params["rng_state"])
+
+    # Reading CSVs as DataFrames
+    all_df = []
+    warn_df = []
+    no_warn_df = []
+    for file in Path(params["csv_files_folder"]).glob("*.csv"):
+        try:
+            df = pandas.read_csv(file)
+            df = df.sort_values(params["time_column"])
+            label_obj = df[params["label_column"]].to_numpy()
+            data_obj = df.drop(columns=params["columns_to_remove"] + [params["time_column"]] + [params["label_column"]])
+            new_obj = {"X": data_obj, "Y": label_obj}
+            if sum(df[params["label_column"]]) > 0:
+                warn_df.append(new_obj)
+                print("\t\tFile Contains warnings")
+            else:
+                no_warn_df.append(new_obj)
+            all_df.append(new_obj)
+            if bool(params["verbose"]):
+                print(f"Read CSV '{file.name}' of {len(df)} rows")
+        except Exception as e:
+            print("Error while processing file %s" % file)
+    print("Found %d files containing warnings" % len(warn_df))
+
+    # Setting up datasets
+    split_index = int(len(warn_df)*float(params["tt_split"]))
+    train_sequences = warn_df[:split_index]
+    test_sequences = warn_df[split_index:]
+    contamination = numpy.average([numpy.average(x["Y"]) for x in train_sequences])
+
+    # Setting Up Analysis - Selection of Classifiers
+    algs = None
+    if params["supervised"]:
+        algs = choose_classifiers(params["sup_algs"], contamination=None, verbose=params["verbose"])
+    else:
+        algs = choose_classifiers(params["uns_algs"], contamination=contamination, verbose=params["verbose"])
+    if params["verbose"]:
+        print("Exercising %s analysis" % ("SUPERVISED" if params["supervised"] else "UNSUPERVISED"))
+
+    # Choosing the type of Analysis
+    predictor = None
+    if not params["use_timeseries"]:
+        predictor = PointWiseAnomalyPredictor(clf_list=algs, supervised=params["supervised"],
+                                              models_folder=params["models_folder"])
+    else:
+        predictor = TimeSeriesAnomalyPredictor(clf_list=algs, supervised=params["supervised"],
+                                              models_folder=params["models_folder"])
+    if params["verbose"]:
+        print("Exercising %s analysis" % ("TIME-SERIES" if params["use_timeseries"] else "NORMAL"))
+
+
+    # Fitting Models
+    predictor.fit(sequences=train_sequences, verbose=params["verbose"])
+
+    # Testing Models
+    test_results = test_models(predictor=predictor, test_sequences=test_sequences, verbose=params["verbose"])
+    if params["print_scores"]:
+        print_scores(to_print=test_results, analysis_tag="test", output_folder=params["output_folder"], filename=params["scores_filename"])
+
+    # Test Models using data with no warnings
+    test_results = test_models(predictor=predictor, test_sequences=no_warn_df, verbose=params["verbose"])
+    if params["print_scores"]:
+        print_scores(to_print=test_results, analysis_tag="test_nowarn", output_folder=params["output_folder"],
+                     filename=params["scores_filename"])
